8858ba56806f4718c2674e85660f1ad8e3f20cc2
Rishikesh1601 <rushikeshmundada1601@gmail.com>
2024-02-05 12:49:06 +0530

    added line-chart and fixed the mobile view

--- index.js
+++ index.js
@@ -1,3 +1,46 @@
+// line chart code
+const xValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const yValues = [24, 26, 20, 10, 20, 30, 21, 26, 29, 30, 12, 18];
+
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "#f05c67",
+      borderColor: "#f05c6726",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: { display: false },
+    scales: {
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Months' // Label for x-axis
+        }
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Attendance' // Label for y-axis
+        },
+        ticks: {
+          min: 0,
+          max: 30
+        }
+      }]
+    }
+  }
+});
+
+
+//chart ends
+
+
 let menuicn = document.querySelector(".menuicn"); 
 let nav = document.querySelector(".navcontainer"); 
 
@@ -15,3 +58,5 @@ let dropdownContent = dropdown.querySelector(".dropdown-content");
         // Toggle the arrow direction
         dropdown.classList.toggle("open");
     });
+
+
